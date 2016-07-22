f3ee949186693cb2abe5247ce0432b55f5be355a
gthsiwamoto <siwamoto@outlook.com>
2016-07-22 23:29:26 +0900

    Modify install-neobundle.sh

--- install-neobundle.sh
+++ install-neobundle.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Install the bundler
+if [ -e ~/.vim/bundle ]; then
+  rm -rf ~/.vim/bundle
+fi
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
